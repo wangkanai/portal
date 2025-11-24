ba8ca0c9de9faae11f439c2457defb269b40113d
wangkanai <sarin@wangkanai.com>
2025-11-24 13:47:33 +0700

    Hash default admin user password in `IdentitySeed` initialization

--- Frontend/Federation/src/Application/Identity/IdentitySeed.cs
+++ Frontend/Federation/src/Application/Identity/IdentitySeed.cs
@@ -1,12 +1,17 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.
 
+using Microsoft.AspNetCore.Identity;
+
 namespace Portal.Federation.Identity;
 
 public static class IdentitySeed
 {
+   private static PasswordHasher<FederationUser> hasher   = new();
+   private static string                         password = hasher.HashPassword(null,"P@ssw0rd");
+
    public static List<FederationUser> Users =>
    [
-      new() { },
+      new() { UserName = "admin", PasswordHash = password},
       new() { }
    ];
 
